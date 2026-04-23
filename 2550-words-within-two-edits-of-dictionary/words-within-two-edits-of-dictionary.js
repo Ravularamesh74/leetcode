db46cfecb8ff193398d5f531dc4f0d8e30a77939
Ravula Ramesh <ravularamesh74@gmail.com>
2026-04-23 07:04:42 +0530

    Time: 11 ms (52.38%) | Memory: 56.1 MB (71.43%) - LeetSync

---
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.js
@@ -0,0 +1,23 @@
+var twoEditWords = function(queries, dictionary) {
+    const res = [];
+
+    for (let q of queries) {
+        for (let d of dictionary) {
+            let diff = 0;
+
+            for (let i = 0; i < q.length; i++) {
+                if (q[i] !== d[i]) {
+                    diff++;
+                    if (diff > 2) break;
+                }
+            }
+
+            if (diff <= 2) {
+                res.push(q);
+                break; // no need to check more dictionary words
+            }
+        }
+    }
+
+    return res;
+};
