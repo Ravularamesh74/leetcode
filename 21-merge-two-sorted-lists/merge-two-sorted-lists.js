d45de18667c4edeb254dd761443886a5c612b481
Ravula Ramesh <ravularamesh74@gmail.com>
2026-04-29 07:22:20 +0530

    Time: 0 ms (100.00%) | Memory: 57.8 MB (43.26%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val === undefined ? 0 : val);
+ *     this.next = (next === undefined ? null : next);
+ * }
+ */
+
+var mergeTwoLists = function(list1, list2) {
+    // Dummy node simplifies handling head
+    let dummy = new ListNode(0);
+    let current = dummy;
+
+    // Traverse both lists
+    while (list1 !== null && list2 !== null) {
+        if (list1.val <= list2.val) {
+            current.next = list1;
+            list1 = list1.next;
+        } else {
+            current.next = list2;
+            list2 = list2.next;
+        }
+        current = current.next;
+    }
+
+    // Attach remaining nodes
+    current.next = list1 !== null ? list1 : list2;
+
+    return dummy.next;
+};
