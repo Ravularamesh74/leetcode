cc9c6b0170c8ed230b46cffbf0e66d9c43595b34
Ravula Ramesh <ravularamesh74@gmail.com>
2026-05-11 00:39:55 +0530

    Time: 0 ms (100.00%) | Memory: 54.7 MB (11.64%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+
+    let length = 0;
+    let i = s.length - 1;
+
+    // Skip trailing spaces
+    while (i >= 0 && s[i] === ' ') {
+        i--;
+    }
+
+    // Count last word length
+    while (i >= 0 && s[i] !== ' ') {
+        length++;
+        i--;
+    }
+
+    return length;
+};
