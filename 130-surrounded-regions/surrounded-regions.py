0971b54e410ca8ec2cac314e12539595d4ce2ade
Ravula Ramesh <ravularamesh74@gmail.com>
2026-06-24 08:07:13 +0530

    Time: 9 ms (47.70%) | Memory: 16.9 MB (29.60%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def solve(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: None Do not return anything, modify board in-place instead.
+        """
+      
+       
+        if not board or not board[0]:
+            return
+
+        m, n = len(board), len(board[0])
+
+        def dfs(i, j):
+            if i < 0 or i >= m or j < 0 or j >= n or board[i][j] != 'O':
+                return
+
+            board[i][j] = 'T'
+
+            dfs(i + 1, j)
+            dfs(i - 1, j)
+            dfs(i, j + 1)
+            dfs(i, j - 1)
+
+        # Mark border-connected regions
+        for i in range(m):
+            dfs(i, 0)
+            dfs(i, n - 1)
+
+        for j in range(n):
+            dfs(0, j)
+            dfs(m - 1, j)
+
+        # Flip surrounded regions and restore marked ones
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+                elif board[i][j] == 'T':
+                    board[i][j] = 'O'
